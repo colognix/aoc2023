5dfb01fa4d98dcc793759a2bc9b970a9ba791908
Markus Neuerbourg <marneuer@gmail.com>
2023-12-02 11:21:06 +0100

    day2 + minor refactoring

--- code/aoc_utils.py
+++ code/aoc_utils.py
@@ -1,5 +1,15 @@
 
-def read_input(path, format):
+# only remove unneccessary information here - further processing is done in day-scripts
+def clean_line(line, day):
+    if day == 1:
+        return line.replace('\n','')
+    if day == 2:
+        return line.replace('\n','').replace(': ',':').replace('Game ','')
+
+def read_input(path_base, day):
+    path = path_base + str(day) + '.txt'
     with open(path,'r') as f:
-        if format == 'plain_lines':
-            return [line.replace('\n','') for line in f.readlines()]
+        if day == 1:
+            return [clean_line(line,day) for line in f.readlines()]
+        if day == 2:
+            return [clean_line(line,day) for line in f.readlines()]
