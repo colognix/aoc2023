cb4645e6337705295fe749f3afbc004eb91989d5
Markus Neuerbourg <marneuer@gmail.com>
2023-12-07 11:17:27 +0100

    day 7 solution + small util refactoring

--- code/aoc_utils.py
+++ code/aoc_utils.py
@@ -1,14 +1,17 @@
 
 # only remove unneccessary information here - further processing is done in day-scripts
 def clean_line(line, day):
+    # linebreaks
+    line = line.replace('\n','')
     if day == 2:
-        line = line.replace('\n','').replace(': ',':').replace('Game ','')
+        line = line.replace(': ',':').replace('Game ','')
     elif day == 4:
-        line = line.replace('\n','').replace('  ',' ').replace(' | ','|').split(':')[1][1:]
+        line = line.replace('  ',' ').replace(' | ','|').split(':')[1][1:]
     elif day == 6:
-        line = line.replace('\n','').split(':')[1].split()
-    else:
-        line = line.replace('\n','')
+        line = line.split(':')[1].split()
+    elif day == 7:
+        line = line.split()
+        line = [line[0],int(line[1])]
     return line
 
 def read_input(path_base, day):
